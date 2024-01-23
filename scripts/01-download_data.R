8f23ccae014a6ad7fdf5481dd608006dd71d5e8d
aryamansuri <77459064+aryamansuri@users.noreply.github.com>
2024-01-23 08:10:22 -0500

    Update 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,28 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from the OpenDataToronto portal
+# Author: Aryaman Suri
+# Date: 22 January 2024
+# Contact: aryaman.suri2mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: none
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
-
+raw_homicide_data <-
+  read_csv(
+    file = 
+      "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/7d72bbbe-8adc-4b36-8ad1-5359f1c7a9cc/resource/f7076075-31c8-4dca-a3ce-967b887e511f/download/Homicides%20-%204326.csv",
+    show_col_types = FALSE,
+    skip = 1
+  )
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(raw_homicide_data, "inputs/data/raw_homicide_data.csv") 
+
+
 
          
